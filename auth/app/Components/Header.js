0df3c2435a8f4be094aa3579fecfadc193c310ed
spoko22 <mic.rad20@gmail.com>
2017-04-10 23:52:14 +0200

    Initial commit for auth project + some files copied from HelloWorld project

---
+++ auth/app/Components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{ props.headerText }</Text>
+    </View>
+  );
+};
+
+
+const styles = {
+  viewStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#eeebeb',
+    height: 60,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+export default Header;
